b691ca6c2e77f7ce9da00fa7c8a1155599f4907e
Dmitrii0777 <patrrriot991@gmail.com>
2025-12-14 19:25:42 +0200

    feat(ui): create Button component

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,57 @@
+import clsx from "clsx";
+
+import type { ButtonHTMLAttributes } from "react";
+
+export type ButtonVariant = "purple" | "secondary" | "dark" | "danger";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  variant?: ButtonVariant;
+  isLoading?: boolean;
+  fullWidth?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  variant = "purple",
+  isLoading = false,
+  fullWidth = false,
+  className,
+  disabled,
+  ...props
+}) => {
+  const baseStyle =
+    "px-4 sm:px-5 py-3 rounded-full flex items-center gap-2 md:gap-4 heading-s-variant transition-colors duration-300";
+
+  const variantStyle = {
+    purple:
+      "bg-[var(--color-purple)] text-white hover:bg-[var(--color-purple-light)] disabled:bg-[var(--color-purple-light)]",
+    dark: "bg-[var(--color-gray-600)] text-[var(--text-tertiary)] hover:bg-[var(--color-draft-btn-hover)]",
+    secondary:
+      "bg-[var(--color-secondary)] text-text-tertiary hover:bg-[var(--color-secondary-btn-hover)]",
+    danger:
+      "bg-[var(--color-danger-200)] text-white hover:bg-[var(--color-danger-100)] disabled:bg-[var(--color-danger-100)]",
+  };
+
+  const buttonClass = clsx(
+    baseStyle,
+    variantStyle[variant],
+    fullWidth && "w-full",
+    disabled || isLoading ? "cursor-not-allowed opacity-60" : "cursor-pointer",
+    className,
+  );
+
+  return (
+    <button className={buttonClass} disabled={disabled || isLoading} {...props}>
+      {isLoading ? (
+        <span className="flex items-center justify-center gap-2">
+          <span>Loading...</span>
+        </span>
+      ) : (
+        children
+      )}
+    </button>
+  );
+};
+
+export default Button;
