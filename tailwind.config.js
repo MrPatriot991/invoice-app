497b1b32d50af8cd7d02459fe4071142787fb230
Dmitrii0777 <patrrriot991@gmail.com>
2025-12-14 15:04:20 +0200

    style: configure Tailwind with custom colors and base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+
+      // Background colors
+      colors: {
+        main: "var(--bg-main)",
+        container: "var(--bg-container)",
+        table: "var(--bg-table-header)",
+      },
+
+      // Text colors
+      text: {
+        primary: "var(--text-primary)",
+        secondary: "var(--text-secondary)",
+        tertiary: "var(--text-tertiary)",
+      },
+
+      // Primari
+      purple: "var(--color-purple)",
+      purpleLight: "var(--color-purple-light)",
+
+      // Draft
+      draft: "var(--color-draft)",
+
+      // Danger
+      danger: "var(--color-danger-200)",
+      dangerLight: "var(--color-danger-100)",
+    },
   },
   plugins: [],
 };
