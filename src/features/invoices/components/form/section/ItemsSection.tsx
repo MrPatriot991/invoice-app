caab68931075e3e47969d1bc44593ac1ed6804b6
Dmitrii0777 <patrrriot991@gmail.com>
2025-12-22 12:56:16 +0200

    fix(invoice-form): safely calculate total using fallback values

--- src/features/invoices/components/form/section/ItemsSection.tsx
+++ src/features/invoices/components/form/section/ItemsSection.tsx
@@ -11,6 +11,7 @@ export const ItemsSection = () => {
   const {
     register,
     control,
+    watch,
     formState: { errors },
   } = useFormContext<InvoiceFormType>();
 
@@ -19,80 +20,84 @@ export const ItemsSection = () => {
     name: "items",
   });
 
-  console.log(errors);
-
   const hasItemErrors = errors.items?.some((item) => item?.qty || item?.price);
 
   return (
     <div>
       <h3 className="mb-4 text-lg text-gray-500">Item List</h3>
 
-      {fields.map((item, index) => (
-        <div
-          key={index}
-          className="mb-4 grid grid-cols-9 gap-4 sm:grid-cols-12"
-        >
-          <div className="col-span-9 sm:col-span-5">
-            <Input
-              label="Item Name"
-              {...register(`items.${index}.name`)}
-              error={errors.items?.[index]?.name?.message}
-            />
-          </div>
-          <div className="col-span-2 justify-center">
-            <Controller
-              name={`items.${index}.qty`}
-              control={control}
-              rules={{
-                required: "Quantity is required",
-                min: { value: 1, message: "Quantity must be at least 1" },
-              }}
-              render={({ field }) => (
-                <Input
-                  className="[appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-                  label="Qty."
-                  type="number"
-                  {...field}
-                  value={field.value ?? ""}
-                  onChange={(e) =>
-                    field.onChange(
-                      e.target.value === ""
-                        ? undefined
-                        : Number(e.target.value),
-                    )
-                  }
-                  error={errors.items?.[index]?.qty?.message}
-                />
-              )}
-            />
-          </div>
-          <div className="col-span-3 sm:col-span-2">
-            <Input
-              className="[appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-              label="Price"
-              type="number"
-              {...register(`items.${index}.price`, { valueAsNumber: true })}
-              error={errors.items?.[index]?.price?.message}
-            />
-          </div>
-          <div className="col-span-3 mb-3 flex items-end justify-center sm:col-span-2">
-            <span className="heading-s-variant text-tertiary transition-colors duration-300">
-              {item.total.toFixed(2)}
-            </span>
-          </div>
+      {fields.map((_, index) => {
+        const qty = watch(`items.${index}.qty`) || 0;
+        const price = watch(`items.${index}.price`) || 0;
+        const total = qty * price;
 
-          {/* Btuun Delete Item */}
-          <div className="col-span-1 flex items-end">
-            <button
-              type="button"
-              className="mb-[10px] px-1 text-gray-400 hover:text-danger"
-              onClick={() => remove(index)}
-            >
-              <Trash2 className="h-6 w-6" />
-            </button>
+        return (
+          <div
+            key={index}
+            className="mb-4 grid grid-cols-9 gap-4 sm:grid-cols-12"
+          >
+            <div className="col-span-9 sm:col-span-5">
+              <Input
+                label="Item Name"
+                {...register(`items.${index}.name`)}
+                error={errors.items?.[index]?.name?.message}
+              />
+            </div>
+            <div className="col-span-2 justify-center">
+              <Controller
+                name={`items.${index}.qty`}
+                control={control}
+                rules={{
+                  required: "Quantity is required",
+                  min: { value: 1, message: "Quantity must be at least 1" },
+                }}
+                render={({ field }) => (
+                  <Input
+                    className="[appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+                    label="Qty."
+                    type="number"
+                    {...field}
+                    value={field.value ?? ""}
+                    onChange={(e) =>
+                      field.onChange(
+                        e.target.value === ""
+                          ? undefined
+                          : Number(e.target.value),
+                      )
+                    }
+                    error={errors.items?.[index]?.qty?.message}
+                  />
+                )}
+              />
+            </div>
+            <div className="col-span-3 sm:col-span-2">
+              <Input
+                className="[appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+                label="Price"
+                type="number"
+                {...register(`items.${index}.price`, { valueAsNumber: true })}
+                error={errors.items?.[index]?.price?.message}
+              />
+            </div>
+            <div className="col-span-3 mb-3 flex items-end justify-center sm:col-span-2">
+              <span className="heading-s-variant text-tertiary transition-colors duration-300">
+                {total.toFixed(0)}
+              </span>
+            </div>
+
+            {/* Btuun Delete Item */}
+            <div className="col-span-1 flex items-end">
+              <button
+                type="button"
+                className="mb-[10px] px-1 text-gray-400 hover:text-danger"
+                onClick={() => remove(index)}
+              >
+                <Trash2 className="h-6 w-6" />
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
 
       {/* Add Item */}
       <Button
